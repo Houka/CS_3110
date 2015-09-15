4cf11c765f034e44f05d88952708760c2628d7d2
Houka <cl795@cornell.edu>
2015-09-15 16:01:01 -0400

    Finished up lab 4

--- lab4/lab4.ml
+++ lab4/lab4.ml
@@ -46,9 +46,35 @@ let exists_other p l =
 *A matrix is square if the number of rows is equal to the number of columns.*)
 let is_square l =
   let row = List.length l in
-  let col = List.fold_right (fun x a -> if List.length x > a then List.length x else a) l 0 in
-  row = col
+  List.fold_right ( fun col a -> a && row = (List.length col)) l true
+
 
 (*adds two matices. If the two input matrices are not the same size, the behavior is unspecified*)
 let add_matices l l' =
-  List.fold_right (fun x a -> x) l []
+  List.map2 (fun x y -> List.map2 (fun a b -> a+b) x y) l l'
+
+(*
+Implement a function multiply_matrices:
+int list list -> int list list -> int list list
+that returns the matrix product of the two input matrices.
+If the two input matrices are not of sizes that can be multiplied together,
+the behavior is unspecified.
+*)
+let col_nth l n = List.fold_right (fun x a -> List.nth x n :: a) l []
+let rec flip_matrix l n=
+  if n > 0 then
+    col_nth l (n-1) :: flip_matrix l (n-1)
+  else
+    []
+let dot_product l l' =
+  let multiplied_list = (List.map2 (fun x y -> x*y) l l') in
+  let result = List.fold_right (fun x a -> x+a) multiplied_list 0 in
+  result
+let dot_product_list l matrix =
+  List.fold_left (fun a x -> dot_product l x :: a) [] matrix
+let multiply_matrices l l' =
+  let flipped = flip_matrix l' (List.length (List.hd l')) in
+  List.fold_right (fun x a -> dot_product_list x flipped::a) l []
+
+
+
